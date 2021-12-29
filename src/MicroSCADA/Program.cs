e4dc7a46d5074ccc9b4ab960744f0025c0b7947c
Fahad Ansari <fahadfreid@gmail.com>
2021-12-28 20:19:11 -0500

    First Check-in.

---
+++ src/MicroSCADA/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Opc.UaFx;
+using Opc.UaFx.Client;
+
+
+var client = new OpcClient("opc.tcp://laptop-jvk86rqt:51210/UA/SampleServer");
+client.Security.EndpointPolicy = new OpcSecurityPolicy(
+        OpcSecurityMode.Sign, OpcSecurityAlgorithm.Basic256Sha256);
+
+var certificate = OpcCertificateManager.CreateCertificate(client);
+client.Certificate = certificate;
+
+client.Connect();
+
+var node = client.BrowseNode(OpcObjectTypes.ObjectsFolder);
+
+BrowseNodes browseNodes = new BrowseNodes();
+browseNodes.Browse(node);
+
+class BrowseNodes
+{
+    public List<OpcNodeInfo> opcNodes = new List<OpcNodeInfo>();
+    public int nodeCount = 0;
+    public void Browse(OpcNodeInfo node, int level = 0)
+    {
+        try
+        {
+            while (level < 4)
+            {
+
+                //opcNodes.Append(node);
+                //node = (OpcNodeInfo)opcNodes.TakeLast(1);
+                Console.WriteLine("{0}{1}: {2} ({3})",
+                        new string('_', level * 4),
+                        node.Attribute(OpcAttribute.DisplayName).Value,
+                        node.Attribute(OpcAttribute.Value)?.Value,
+                        node.NodeId);
+                level++;
+
+
+                foreach (var childNode in node.Children())
+                {
+                    Browse(childNode, level);
+                    
+                }
+
+                //Console.ReadLine();
+            }
+        }
+        catch (Exception ex)
+        { 
+            Console.WriteLine("\n\n"+ex.ToString()+"\n\n"); 
+        }
+
+        
+
+
+        //foreach (var childNode in node.Children())
+        //    Browse(childNode, level);
+    }
+}
---
+++ src/MicroSCADA_Client/Data/OPCUABrowse.cs
@@ -0,0 +1,113 @@
+﻿using Opc.UaFx;
+using Opc.UaFx.Client;
+using static MicroSCADA_Client.Pages.Index;
+
+
+namespace MicroSCADA_Client.Data
+{
+    public class OPCNodeObject
+    {
+        public string? Id { get; set; }
+        public string? NodeName { get; set; }
+        public string? NodeValue { get; set; }
+
+        public OPCNodeObject (string? id, string? nodeName, string? nodeValue)
+        {
+            Id = id;
+            NodeName = nodeName;
+            NodeValue = nodeValue;
+        }
+    }
+    public static class OPCUABrowse
+    {
+        #region Properties    
+        public static string UAendpointAddress { get; set; } = "opc.tcp://laptop-jvk86rqt:51210/UA/SampleServer";
+
+        public static List<OPCNodeObject>? opcNodes { get; private set; } = new List<OPCNodeObject>() { new OPCNodeObject("0", "Initialize", "0")};
+        public static OpcClient Client { get; set; }
+
+        public static bool ConnectionEstabilished { get; set; } = false;
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Initializes the OPC UA connection by creating a certificate and then connecting to the given UAendpointAddress
+        /// </summary>
+        public static void Initialize()
+        {
+            Client = new OpcClient(UAendpointAddress);
+
+            //might need to expand this by putting a try catch around it and trying
+            //all security methods instead of hardcoding a specific one
+            Client.Security.EndpointPolicy = new OpcSecurityPolicy(OpcSecurityMode.Sign, OpcSecurityAlgorithm.Basic256Sha256);
+
+            var certificate = OpcCertificateManager.CreateCertificate(Client);
+            Client.Certificate = certificate;
+
+            try
+            {
+                Client.Connect();
+                var node = Client.BrowseNode(OpcObjectTypes.ObjectsFolder);
+                Browse(node);
+                ConnectionEstabilished = true;
+            }
+            catch (OpcException ex)
+            {
+                Console.WriteLine("Failed to Connect: " + ex.Message);
+            }
+        }
+
+        private static bool Browse(OpcNodeInfo node)
+        {
+            //if (opcNodes is not null) opcNodes.Clear();
+
+            var result = false;
+
+            try
+            {
+
+                if (node is OpcObjectNodeInfo)
+                {
+                    if (node.Reference.TypeDefinitionId == Opc.Ua.ObjectTypeIds.FolderType) { }
+                }
+                else if (node is OpcMethodNodeInfo)
+                {
+
+                }
+                else if (node is OpcVariableNodeInfo)
+                {
+                    if (node.Reference.ReferenceType == OpcReferenceType.HasProperty) { }
+                }
+
+                opcNodes?.Add
+                    ( new OPCNodeObject(node.NodeId.ToString(), node.Name.ToString(), node.Attribute(OpcAttribute.Value)?.Value.ToString()));
+
+                result = true;
+            }
+            catch (OpcException ex)
+            {
+                Console.WriteLine("Failed to browse: " + ex.Message);
+            }
+
+            return result;
+        }
+
+        public static void HandleNodesTreeViewAfterExpand(string opcnodeid)
+        {
+            OpcNodeInfo machineNode = Client.BrowseNode(opcnodeid);
+            opcNodes?.Clear();
+
+            foreach (var childNode in machineNode.Children())
+            {
+                if (!Browse(childNode))
+                    break;
+
+                opcNodes.Add
+                    (new OPCNodeObject(childNode.NodeId.ToString(), childNode.Name.ToString(), childNode.Attribute(OpcAttribute.Value)?.Value.ToString()));
+
+            }
+        }
+        #endregion
+    }
+}
